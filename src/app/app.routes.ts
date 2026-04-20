3a9afb6aed75f7506945c6e811f34f458ad69b0d
kpavel83 <k.pavel83@mail.ru>
2026-04-20 21:42:34 +0000

    project

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Order } from './pages/order/order';
+import { Track } from './pages/track/track';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: Home
+  },
+  {
+    path: 'order',
+    component: Order
+  },
+  {
+    path: 'track',
+    component: Track
+  },
+  {
+    path: '**',
+    redirectTo: ''
+  }
+];
